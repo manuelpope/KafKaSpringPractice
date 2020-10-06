b1fbefd93f6b7fcd17916881b7aa06db6d045fb0
manueltobon <manuelpope@gmail.com>
2020-10-06 08:54:00 -0300

    first commit

---
+++ demo/src/main/java/com/example/demo/DemoApplicationCallback.java
@@ -0,0 +1,72 @@
+package com.example.demo;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaProducerException;
+import org.springframework.kafka.core.KafkaSendCallback;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.concurrent.ListenableFuture;
+
+@SpringBootApplication
+@Controller
+@ComponentScan
+public class DemoApplicationCallback implements CommandLineRunner {
+
+
+    @Autowired
+    private KafkaTemplate <Integer, String>kafkaTemplate;
+
+
+    private static final Logger log = LoggerFactory.getLogger(DemoApplicationCallback.class);
+
+    @KafkaListener(topics = "test", groupId = "group-Test-spring")
+    public void listenTo(String message){
+    log.info("message receive {}", message);
+
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplicationCallback.class, args);
+
+    }
+
+
+//Method  callback async , intend to know what happened before.
+    @Override
+    public void run(String... args) throws Exception {
+        ListenableFuture<SendResult<Integer, String>> future =
+                kafkaTemplate.send("test", "1");
+        future.addCallback(new KafkaSendCallback<Integer, String>() {
+
+
+                               @Override
+                               public void onSuccess(SendResult<Integer, String> result) {
+                                   log.info("sent"+ result.getRecordMetadata().offset());
+                               }
+
+                               @Override
+                               public void onFailure(Throwable ex) {
+                                   log.info("error", ex);
+                               }
+
+                               @Override
+                               public void onFailure(KafkaProducerException ex) {
+                                   log.info(ex.getMessage());
+                               }
+                           }
+        );
+
+    }
+
+
+}
